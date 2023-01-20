c27974199d106077b653e95fbeaab75cb59f4aaa
Stephan Köninger <github@stekoe.de>
2023-01-20 20:05:00 +0100

    initial

---
+++ src/components/CounterVue.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+      <div class="counter">
+        <button @click="count--">-</button>
+        <pre v-text="count"></pre>
+        <button @click="count++">+</button>
+      </div>
+      <div class="counter-message">
+        <slot></slot>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+.counter {
+	display: grid;
+	font-size: 2em;
+	grid-template-columns: repeat(3, minmax(0, 1fr));
+	place-items: center;
+}
+
+.counter-message {
+	text-align: center;
+}
+
+</style>
