cf0fba2beb9393c655529d79a73f1c3e463cfc4c
Stephan Köninger <github@stekoe.de>
2023-01-30 16:09:09 +0100

    test

--- src/pages/htmx.json.ts
+++ src/pages/htmx.json.ts
@@ -1,9 +1,8 @@
 import type {APIRoute} from "astro";
 
 export const post: APIRoute = async ({ request }) => {
-    const formData = await request.formData();
-
+    const prompt = request.headers.get("hx-prompt");
     return {
-        body: `User entered <i>${formData.get("submission")}</i>`
+        body: `User entered <i>${prompt}</i>`
     }
 }
