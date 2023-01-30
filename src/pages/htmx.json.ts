5d40e713e3c172edcc5c5b2dad1efe2865c12c85
Stephan Köninger <github@stekoe.de>
2023-01-30 15:55:49 +0100

    test

--- src/pages/htmx.json.ts
+++ src/pages/htmx.json.ts
@@ -4,6 +4,6 @@ export const post: APIRoute = async ({ request }) => {
     const formData = await request.formData();
 
     return {
-        body: `User entered <i>${formData.get("response")}</i>`
+        body: `User entered <i>${formData.get("submission")}</i>`
     }
 }
