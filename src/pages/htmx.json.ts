586de7b5e3e8e440097496bc5fd7c4f3b569f2e9
Stephan Köninger <github@stekoe.de>
2023-01-30 15:37:44 +0100

    test

--- src/pages/htmx.json.ts
+++ src/pages/htmx.json.ts
@@ -1,13 +1,11 @@
 import type {APIRoute} from "astro";
 
 export const post: APIRoute = async ({ request }) => {
-    if (request.headers.get("Content-Type") === "application/json") {
-        const body = await request.json();
-        console.log(body);
-        const name = body.name;
-        return new Response(`User entered <i>${name}</i>`, {
-            status: 200
-        })
+    const body = await request.json();
+    console.log(body);
+    const name = "body.name";
+
+    return {
+        body: `User entered <i>${name}</i>`
     }
-    return new Response(null, { status: 400 });
 }
